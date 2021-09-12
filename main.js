e59f693e235938f1cdd375fca234f897524f8fdc
Szegoo <sakacszergej@gmail.com>
2021-09-12 20:06:55 +0200

    commit

--- main.js
+++
@@ -1,7 +0,0 @@
-const PowergateIO = require('orbit-db-powergate-io')
- 
-const host = 'http://0.0.0.0:6002' // This is the default value
-PowergateIO.create(host)
-  .then((powergateio) => {
-    console.log(powergateio.wallet) // Will be {} until funded
-  })
